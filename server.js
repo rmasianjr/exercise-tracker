36c120b7176c1d77b054ac7f7b86231a3be5de17
Ricardo Masian Jr <ricardo.masianjr@gmail.com>
2019-01-29 17:58:33 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+mongoose.Promise = global.Promise;
+mongoose
+  .connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/exercise_track_db',
+    { useNewUrlParser: true }
+  )
+  .catch(err => console.log(`Error: ${err.message}`));
+
+app.use(cors());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
