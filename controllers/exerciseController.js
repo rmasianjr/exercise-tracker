dc9b291afac7263a36795a7802e6fbc5c2a1a633
Ricardo Masian Jr <ricardo.masianjr@gmail.com>
2019-02-01 15:40:18 +0800

    Add POST /api/exercise/add

--- controllers/exerciseController.js
+++ controllers/exerciseController.js
@@ -17,3 +17,29 @@ exports.createUser = async (req, res) => {
   const { username: name, _id } = newUser;
   res.send({ username: name, _id });
 };
+
+exports.addExercise = async (req, res) => {
+  const { userId, description, duration, date } = req.body;
+  const user = await User.findById(userId);
+
+  if (!user) {
+    return res.status(404).send({ error: 'No user found in the given id' });
+  }
+
+  const exercise = date
+    ? { description, duration, date }
+    : { description, duration };
+
+  user.log.push(exercise);
+  await user.save();
+
+  const log = user.log[user.log.length - 1];
+
+  res.send({
+    username: user.username,
+    description: log.description,
+    duration: log.duration,
+    _id: user._id,
+    date: log.date.toDateString()
+  });
+};
--- routes/exercise.js
+++ routes/exercise.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const { createUser } = require('../controllers/exerciseController');
+const {
+  createUser,
+  addExercise
+} = require('../controllers/exerciseController');
 
 const catchErrors = require('../middleware/catchErrors');
 
 router.post('/new-user', catchErrors(createUser));
+router.post('/add', catchErrors(addExercise));
 
 module.exports = router;
